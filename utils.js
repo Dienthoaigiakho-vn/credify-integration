d7ca8babdf7154329326f2ac3c614d4b9348aed3
PhamGiaHuong <>
2022-12-29 15:32:23 +0700

    sc-37009: core-phase-2-add-the-creating-intent-to-the

--- utils.js
+++ utils.js
@@ -40,9 +40,96 @@ const allowCrossDomain = (req, res, next) => {
   })(req, res, next);
 };
 
+const buildPostIntentsPayload = (req) => {
+  /**
+   * Required if type is OFFER.
+   * {Enum} "BNPL" | "OFFER"
+   * @example BNPL
+   */
+  const type = req.body.type;
+
+  const bnplOrder = {
+    ...req.body.bnpl_order,
+    order_lines: mapCategoryFromOrderLine(req.body.bnpl_order.order_lines),
+  };
+  console.log("bnplOrder", mapCategoryFromOrderLine(req.body.bnpl_order.order_lines));
+  /**
+   * Required if type is OFFER.
+   * {object}
+   * @example
+   *  {
+      "offer_code": "string",
+      "product_code": "string"
+    }
+  */
+  const offer = null;
+
+  /**
+   * Required if type is OFFER.
+   * {object}
+   * @example
+   *   {
+        "id": "string",
+        "credify_id": "38e67621-b57e-4eb1-b2ae-66eb3f9817f0",
+        "name": "string",
+        "email": "string",
+        "phone": {
+        "phone_number": "string",
+        "country_code": "+84"
+        },e"
+        "gender": "male"
+    }
+  */
+
+  /**
+   * Required if type is OFFER.
+   * {Object<UserInfo>}
+   * @example
+   *   {
+        "id": "string",
+        "credify_id": "38e67621-b57e-4eb1-b2ae-66eb3f9817f0",
+        "name": "string",
+        "email": "string",
+        "phone": {
+        "phone_number": "string",
+        "country_code": "+84"
+        },e"
+        "gender": "male"
+    }
+  */
+  const user = req.body.user;
+
+  /**
+   * {Object<ServiceInfo>}
+   * @example
+   *  {
+        "available_provider_ids": [
+          "497f6eca-6276-4993-bfeb-53cbbbba6f08"
+        ],
+        "available_product_codes": [
+          "string"
+        ],
+        "available_offer_codes": [
+          "string"
+        ],
+        "product_code": "string",
+        "offer_code": null,
+        "package_code": "string",
+        "ui": {
+          "theme": {},
+          "locale": "vi-VN"
+        }
+      }
+  */
+  const service = req.body.service;
+
+  return { type, bnplOrder, offer, service, user };
+};
+
 module.exports = {
   formKey,
   getCategoryItem,
   mapCategoryFromOrderLine,
   allowCrossDomain,
+  buildPostIntentsPayload,
 };
